4420bcca0108bd4301c5d8bb164a51b3a4ec579f
megafayray <fay.brotherston@gmail.com>
2025-01-16 11:03:02 -0500

    Write instance variables to file when escape button is pressed

--- lib/display.rb
+++ lib/display.rb
@@ -20,19 +20,36 @@ class Display
     puts @spaces.join(' ')
   end
 
+  def save_game? #maybe this should be private
+    if @input == "\e"
+      savedgame = File.new('savedgame.txt', 'w')
+      savedgame.puts File.mtime 'savedgame.txt'
+      savedgame.puts "Letters guessed: #{@gameplay.letters_guessed}"
+      savedgame.puts "Number of incorrect guesses: #{@gameplay.incorrect_guesses}"
+      savedgame.puts "Current saved spaces to display: #{@spaces.join(' ')}"
+
+      savedgame.close
+    end
+  end
+
   def request_guess
-    input = gets.chomp.downcase
-    if @gameplay.secret_word.include?(input)
+    puts "To save the game, press the Esc button. Otherwise, guess a letter"
+    @input = gets.chomp.downcase
+    save_game?
+    if @gameplay.secret_word.include?(@input)
       @gameplay.secret_word.chars.each_with_index do | char, index | #.chars method converts a string into an array of its individual characters
-        if char == input
-          @spaces[index] = input
+        if char == @input
+          @spaces[index] = @input
         end
       end
-    else
+    elsif @input != "\e"
       @gameplay.incorrect_guesses += 1 
     end
 
-    @gameplay.letters_guessed << input
+    if @input != "\e"
+      @gameplay.letters_guessed << @input
+    end
+
     info
     puts @spaces.join(' ')
   end
